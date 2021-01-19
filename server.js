3ba717c55b13a0dfb0a432f6a6d5b2f986851406
Gerard Keane <gerard.keane@dfkeane.com>
2021-01-19 00:47:06 +0000

    hopefully fixed return object

--- server.js
+++ server.js
@@ -53,9 +53,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/exercise/new-user", async (req, res) => {
-  // You can POST to /api/exercise/new-user with form data username to create a new user.
-  // The returned response will be an object with username and _id properties.
-  // Returns {"username":"bobbyb","_id":"600478940aa40e05f2b893ff"}
   console.log(req.body);
   let username = req.body.username;
 
@@ -74,19 +71,12 @@ app.post("/api/exercise/new-user", async (req, res) => {
 });
 
 app.get("/api/exercise/users", async (req, res) => {
-  // You can make a GET request to api/exercise/users to get an array of all users.
-  // Each element in the array is an object containing a user's username and _id.
   const all = await User.find();
 
   res.send(all);
 });
 
 app.post("/api/exercise/add", async (req, res) => {
-  // You can POST to /api/exercise/add with form data userId=_id, description, duration,
-  // and optionally date. If no date is supplied, the current date will be used.
-  // The response returned will be the user object with the exercise fields added.
-  // Return {"_id":"6004774f0aa40e05f2b893fc","username":"bobby","date":"Thu Jan 16 2020","duration":3,"description":"Jogging"}
-  console.log(req.body);
   const { userId, description, duration, date } = req.body;
 
   const payload = {
@@ -120,15 +110,6 @@ app.post("/api/exercise/add", async (req, res) => {
 });
 
 app.get("/api/exercise/log", (req, res) => {
-  // You can make a GET request to /api/exercise/log with a parameter of userId=_id
-  // to retrieve a full exercise log of any user. The returned response will be the user
-  // object with a log array of all the exercises added. Each log item has the description,
-  // duration, and date properties.
-  // A request to a user's log (/api/exercise/log) returns an object with a count property
-  // representing the number of exercises returned.
-  // You can add from, to and limit parameters to a /api/exercise/log request to retrieve
-  // part of the log of any user. from and to are dates in yyyy-mm-dd format.
-  // limit is an integer of how many logs to send back.
   const { userId, from, to, limit } = req.query;
 
   User.findById({ _id: userId }, (err, doc) => {
@@ -137,8 +118,8 @@ app.get("/api/exercise/log", (req, res) => {
     } else {
       let data = doc.log;
 
-      let fromDate = isValidDate(new Date(from)) ? new Date(req.query.from) : new Date(data[0].date);
-      let toDate = isValidDate(new Date(to)) ? new Date(req.query.to) : new Date();
+      let fromDate = isValidDate(new Date(from)) ? new Date(from) : new Date(data[0].date);
+      let toDate = isValidDate(new Date(to)) ? new Date(to) : new Date();
       let itemLimit = limit ? Number(limit) : Number(data.length);
 
       console.log("from: ", fromDate, "to: ", toDate, "limit: ", limit);
@@ -148,11 +129,12 @@ app.get("/api/exercise/log", (req, res) => {
         .filter((exercise) => exercise.date >= fromDate)
         .slice(0, itemLimit);
 
-      // Add count of returned exercises
-      data["count"] = data[0].length;
-
-      console.log("filtered: ", data);
-      res.send({ log: data });
+      res.json({
+        _id: userId,
+        username: doc.username,
+        count: data.length,
+        log: data,
+      });
     }
   });
 });
